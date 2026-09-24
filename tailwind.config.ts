f2fd6651313f809d1a55412588c2d45f633047fb
D3 Officiel <desire0369@gmail.com>
2026-09-24 19:29:26 +0000

    ajuste la a 20 secondes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -145,7 +145,7 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in-scale': 'fade-in-scale 1.2s cubic-bezier(0.25, 1, 0.5, 1) forwards',
         'ping-slow-once': 'ping-slow-once 1.5s cubic-bezier(0, 0, 0.2, 1) forwards',
-        'spin-slow': 'spin-slow 12s linear infinite',
+        'spin-slow': 'spin-slow 20s linear infinite',
         'pulse-subtle': 'pulse-subtle 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         'ripple-1': 'ripple-1 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         'ripple-2': 'ripple-2 2s cubic-bezier(0.4, 0, 0.6, 1) infinite 0.5s',
