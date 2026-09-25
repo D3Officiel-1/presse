8c5b59d38947dd998d2b9d5fc37a465395a2ff9d
D3 Officiel <desire0369@gmail.com>
2026-09-25 19:43:47 +0000

    Try fixing this error: `Runtime Error: Element type is invalid: expected

--- src/components/zap-navigation.tsx
+++ src/components/zap-navigation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/navigation';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Home, Users, Plus, MessageCircle, User } from 'lucide-react';
 import { cn } from '@/lib/utils';
